2a3ce709626b5cc4d1e18dec7a600bedac881730
zerkc <gustavo.a.g.latorre@gmail.com>
2022-02-10 01:04:22 -0300

    normativas y pagina de diseñadores

--- pages/designers.tsx
+++ pages/designers.tsx
@@ -1,14 +1,60 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { BodyComponent } from '../components/BodyComponent/BodyComponent'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Streamers.module.scss'
 
-const Streamers: NextPage = () => {
+const Designers: NextPage = ({streamers} : any) => {
   return (
     <BodyComponent>
-      <h1>pagina en contruccion</h1>
+      <h1 className={styles.title}>Nuestros Diseñadores</h1>
+      <section className={styles.Section}>
+        {streamers.map((e:any)=>{
+          return (
+            <div key={e.src} className={styles.card}>
+              <img src={e.src} />
+              <div className={styles.contentContainer}>
+                <h3 className={styles.ctitle}>{e.title}</h3>
+                <div className={styles.content} dangerouslySetInnerHTML={{__html:e.content}}></div>
+              </div>
+            </div>
+          )
+        })}
+        <img />        
+      </section>
     </BodyComponent>
   )
 }
 
-export default Streamers
+export async function getStaticProps() {
+  
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      streamers:[
+        {
+          src:"/images/Rodrigo-Corani-Lopez-Logo.png",
+          title:"Audney",
+          content:`
+          <p>Soy Andrea, pero en mi comunidad me conocen como Audney. Soy una mamá gamer de tres niños que intenta ser repostera y mejorar su nivel en videojuegos sin morir en el intento…</p>
+          <p>Si bien no soy experta en el rubro, me gustan muchos los Shooters y MMORPG, siendo mis favoritos; Apex Legends, Valorant, WOW y Guild Wars. También juego LOL, Minecraft, Paladins y hago roleplay en GTA V.</p>
+          <p>Amo compartir con la gente mi gusto por los juegos virtuales, y encontrarme con personas de diversas partes del mundo me llena de emoción.</p>
+          <p>Espero poder conocerlos en esta hermosa comunidad <3</p>
+          `
+        },
+        {
+          src:"/images/Danny02.jpg",
+          title:"Zeikuro",
+          content:`
+          <p>Mi nombre es Christian pero me conocen como ZeiKuro aunque algunos me dicen Shiro. Me encantan los juegos Shooter y los juegos difíciles que representen un reto, por eso mismo soy amante de la Saga Dark Souls. Si bien son estos juegos los que más se adaptan a mi estilo, disfruto de casi todo videojuego existente. Principalmente, los que más horas le doy son: MIR4, League of Legends y, Rainbow Six Siege.</p>
+          <p>Si bien no soy experta en el rubro, me gustan muchos los Shooters y MMORPG, siendo mis favoritos; Apex Legends, Valorant, WOW y Guild Wars. También juego LOL, Minecraft, Paladins y hago roleplay en GTA V.</p>
+          <p>"... sí representa un verdadero desafío lo quiero, necesito intentarlo..."</p>
+          `
+        },
+      ],
+    },
+  }
+}
+
+export default Designers
--- pages/rules.tsx
+++ pages/rules.tsx
@@ -6,7 +6,14 @@ import styles from '../styles/Home.module.css'
 const Streamers: NextPage = () => {
   return (
     <BodyComponent>
-      <h1>pagina en contruccion</h1>
+      <h1>Normas de convivencia</h1>
+      <p style={{textAlign:"justify"}}>1. Queda terminantemente prohibido el agredir a otros miembros de la comunidad. No se tolerarán las faltas de respeto. Política de cero toxicidad. Primer strike: advertencia seguida de una disculpa a los afectados. Segundo Strike: Ban temporal (puede que definitivo) y revisión del caso. Tercer Strike: Ban Irrevocable. </p>
+      <p style={{textAlign:"justify"}}>2. Están prohibidos los comentarios xenófobos, racistas, humor negro, burlarse de atentados y cualquier otro tipo de discriminación. Entendemos que existe el sentido del humor y que dependerá de la confianza que se tenga entre las personas involucradas el uso de comentarios que involucren las prohibiciones antes mencionadas, eso es algo que siempre tendremos en cuenta; no es la búsqueda de esta regla el intervenir en asuntos propios de las mecánicas internas de cada grupo, sino sentar una base para casos particulares de ataques dirigidos a integrantes de la comunidad con el único motivo de agredir, dañar o perjudicar de alguna forma la integridad de la persona. </p>
+      <p style={{textAlign:"justify"}}>3. Está prohibida la caza de brujas. En caso de buscar exponer a una persona o pedir denuncias masivas a perfil/canal/página por un tema relativo a la comunidad o que afecte de forma particular la integridad de algún miembro de la comunidad deberá comunicarse con el Staff a fin de ratificar el apoyo de la administración o bien, ante la negativa, acatarse a la regla.</p>
+      <p style={{textAlign:"justify"}}>4. Sabemos que hay temas que son delicados, por ello si bien no prohibimos hablar de cuestiones que atañen a religión, fútbol, política, sexo, consumo de drogas y estupefacientes. Llamamos a consideración de los integrantes de la comunidad el tener tacto al momento de encarar estos tópicos. Los cuales entendemos, son esenciales de la sociedad y forman parte integral en los juegos. Tengan siempre presente el manejarse con respeto.</p>
+      <p style={{textAlign:"justify"}}>5. Está prohibido publicar contenido pornográfico o cualquier relación a la misma (links, gemidos, orgasmos, audio/video, desnudos explicito o no, cualquier broma relacionada al CP, juguetes sexuales, screen de sitios porno). Con la excepción de aquellos que estén vinculados de forma directa a un juego.</p>
+      <p style={{textAlign:"justify"}}>6. Queda prohibido el Spam de publicaciones en cualquier de nuestras redes sociales, grupos de Whatsapp y Discord. </p>
+      <p style={{textAlign:"justify"}}>7. El uso del nombre Love4Gaming tanto como del logo queda completa y exclusivamente reservado para el personal autorizado desde la administración de nuestra comunidad. Su uso por quienes no cuenten con el aval correspondiente será motivo de acciones legales.</p>
     </BodyComponent>
   )
 }
